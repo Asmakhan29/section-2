d582ccf55a7d659007b26483a080369df8a50e93
Asmakhan29 <asmakhan2942@gmail.com>
2023-11-04 01:46:13 +0530

    react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1 className='my-title'>My React App</h1>
+      <button className='btn btn-primary'>Nice button</button>
+    </>
+  )
+}
+
+export default App
