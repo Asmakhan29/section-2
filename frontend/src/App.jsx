def33d309c083c3697d4c5983ff3b82e6dc65d6e
Asmakhan29 <asmakhan2942@gmail.com>
2023-11-06 14:04:10 +0530

    react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <h1 className='my-title'>My React App</h1>
-      <button className='btn'>Nice button</button>
-    </>
+    <div>
+      
+      
+      <BrowserRouter>
+          <Routes>
+
+          <Route path='/' element={ <Home/> } />
+          <Route path='/Login' element={ <Login/> } />
+          <Route path='/Signup' element={ <Signup/> } />
+
+          </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
